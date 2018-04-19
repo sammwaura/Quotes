644ee0d3520606505172c2ae6528a0c168973189
sammwaura <meshsami27@gmail.com>
2018-04-19 16:13:13 +0300

    add quote component

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
-import { Quotes } from './quotes'
+import { Component, OnInit } from '@angular/core';
+import {Quotes} from '../quotes'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
-export class AppComponent {
+export class QuoteComponent implements OnInit {
+
   quotes = [
     new Quotes(1,'Lightning makes no sound until it strikes'),
     new Quotes(2,'Technique is not being able to juggle a ball 1000 times.Technique is passing the ball with one touch, with the right speed, at the right foot of your team mate.'),
     new Quotes(3,'All empires are created of blood and fire.'),
 
   ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
