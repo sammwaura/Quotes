4a6a77a93baf1a81e8db52f65a0394b41ca9e0cf
sammwaura <meshsami27@gmail.com>
2018-04-19 16:27:06 +0300

    add quote-details

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -9,9 +9,10 @@ import {Quotes} from '../quotes'
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quotes(1,'Lightning makes no sound until it strikes'),
-    new Quotes(2,'Technique is not being able to juggle a ball 1000 times.Technique is passing the ball with one touch, with the right speed, at the right foot of your team mate.'),
-    new Quotes(3,'All empires are created of blood and fire.'),
+    new Quotes(1,'Lightning makes no sound until it strikes', 'by Martin Luther King Jr'),
+    new Quotes(2,'Technique is not being able to juggle a ball 1000 times.Technique is passing the ball with one touch, with the right speed, at the right foot of your team mate.','by Johann Cruff'),
+    new Quotes(3,'All empires are created of blood and fire.','by Pablo Escobar'),
+    
 
   ]
   constructor() { }
