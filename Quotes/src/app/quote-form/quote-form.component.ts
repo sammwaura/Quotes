a8ab4b28280b7030d2b139afea4c3cb8cbda7af6
sammwaura <meshsami27@gmail.com>
2018-04-20 07:43:43 +0300

    add form-quotes component

---
+++ Quotes/src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Quotes} from '../quotes';
+
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+  newQuote=new quotes (0,"","",new Date());
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
